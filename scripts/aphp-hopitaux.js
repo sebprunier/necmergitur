1aca0afe9c1f955d59556b27770ee4d71c648b1d
Sébastien Prunier <sebastien.prunier@gmail.com>
2016-01-15 22:04:14 +0100

    Liste des hopitaux de Paris

---
+++ scripts/aphp-hopitaux.js
@@ -0,0 +1,24 @@
+var cheerio = require('cheerio');
+var request = require('request');
+
+var options = {
+  url: 'http://www.aphp.fr/hopitaux',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+request(options, (err, response, body) => {
+    if (err) throw err;
+
+    var $ = cheerio.load(body);
+
+    $('div.hospital-info').map((i, hospital) => {
+        var hospitalNode = $(hospital);
+        var hospitalName = $('span.hospital-name > a', hospitalNode).text();
+        var hospitalCoordsLink = $('a.map-link', hospitalNode).attr('href');
+        var hospitalCoords = hospitalCoordsLink.split('@')[1].split(',');
+
+        console.log(`${hospitalName};${hospitalCoords[0]};${hospitalCoords[1]}`);
+    });
+});
